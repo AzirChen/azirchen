359852a49c7f6dfe27b6f5931db1f9c6e53bf63f
AzirChen <Azir_Chen@wistronits.com>
2016-12-14 15:02:34 +0800

    finish moveto div

---
+++ js/scrollTo.js
@@ -0,0 +1,27 @@
+/*$(document).ready(function () {
+	$(".about").click(function () {
+	  $("html,body").animate(function(){
+	  	scrollTop:$("#about").offset().top },
+	  	{duration:500,easing:"swing"});
+	  // return false;
+	});
+	$(".skill").click(function () {
+	  $("html,body").animate(function(){
+	  	scrollTop:$("#skill").offset().top },
+	  	800);
+	  return false;
+	});
+	$(".article").click(function () {
+	  $("html,body").animate(function(){
+	  	scrollTop:$("#article").offset().top },
+	  	800);
+	  return false;
+	});
+	$(".contact").click(function () {
+	  $("html,body").animate(function(){
+	  	scrollTop:$("#contact").offset().top },
+	  	800);
+	  return false;
+	});
+});
+*/
