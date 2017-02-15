d3e21b9b60c38e131c1b5531abbb9493a4163883
AzirChen <Azir_Chen@wistronits.com>
2017-02-15 14:44:57 +0800

    scrolltotop and hoverAnimation

--- js/scrollTo.js
+++ js/scrollTo.js
@@ -25,3 +25,11 @@
 	});
 });
 */
+$(window).scroll(function(){
+var wintop = $(window).scrollTop(), docheight = 
+    
+    $(document).height(), winheight = $(window).height();
+            var scrolled = (wintop/(docheight-winheight))*100;
+  
+        $('.scroll-line').css('width', (scrolled + '%'));
+});
